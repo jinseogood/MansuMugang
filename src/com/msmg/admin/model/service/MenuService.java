fb9506634200babeeecc91d41da4966e6fa36dec
Administrator <Administrator@JINSEO-NT>
2018-08-21 15:16:38 +0900

    0821 관리자 메뉴 수정

--- src/com/msmg/admin/model/service/MenuService.java
+++ src/com/msmg/admin/model/service/MenuService.java
@@ -1,5 +1,29 @@
 package com.msmg.admin.model.service;
 
+import java.sql.Connection;
+
+import com.msmg.admin.model.dao.MenuDao;
+import com.msmg.admin.model.vo.Menu;
+
+import static com.msmg.common.JDBCTemplate.*;
+
 public class MenuService {
+
+	public int insertMenu(Menu menu) {
+		Connection con=getConnection();
+		
+		int result=new MenuDao().insertMenu(menu, con);
+		
+		if(result>0){
+			commit(con);
+		}
+		else{
+			rollback(con);
+		}
+		
+		close(con);
+		
+		return result;
+	}
 
 }
--- src/com/msmg/admin/model/vo/Menu.java
+++ src/com/msmg/admin/model/vo/Menu.java
@@ -1,5 +1,66 @@
 package com.msmg.admin.model.vo;
 
-public class Menu {
+public class Menu implements java.io.Serializable{
+	private String menuCode;
+	private String menuName;
+	private String mainMat;
+	private String subMat;
+	private int price;
+	
+	public Menu(){}
+	public Menu(String menuCode, String menuName, String mainMat, String subMat, int price) {
+		super();
+		this.menuCode = menuCode;
+		this.menuName = menuName;
+		this.mainMat = mainMat;
+		this.subMat = subMat;
+		this.price = price;
+	}
 
+	public String getMenuCode() {
+		return menuCode;
+	}
+
+	public String getMenuName() {
+		return menuName;
+	}
+
+	public String getMainMat() {
+		return mainMat;
+	}
+
+	public String getSubMat() {
+		return subMat;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setMenuCode(String menuCode) {
+		this.menuCode = menuCode;
+	}
+
+	public void setMenuName(String menuName) {
+		this.menuName = menuName;
+	}
+
+	public void setMainMat(String mainMat) {
+		this.mainMat = mainMat;
+	}
+
+	public void setSubMat(String subMat) {
+		this.subMat = subMat;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Menu [menuCode=" + menuCode + ", menuName=" + menuName + ", mainMat=" + mainMat + ", subMat=" + subMat
+				+ ", price=" + price + "]";
+	}
+	
 }
