953c630686eac71b6864b2dd1a3157620584f665
jieunkyung <jieunkyung@DESKTOP-V7UA1FK.mshome.net>
2018-08-21 15:19:01 +0900

    정보게시판 댓글

--- src/com/msmg/board/information/controller/SelectOneBoardServlet.java
+++ src/com/msmg/board/information/controller/SelectOneBoardServlet.java
@@ -1,6 +1,7 @@
 package com.msmg.board.information.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.msmg.board.information.model.service.BoardService;
 import com.msmg.board.information.model.vo.Board;
+import com.msmg.board.information.model.vo.Reply;
 
 /**
  * Servlet implementation class SelectOneBoardServlet
@@ -26,16 +28,21 @@ public class SelectOneBoardServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String num = request.getParameter("num");
 		
-		System.out.println(num);
+		System.out.println("selectOne:"+num);
 		
 		Board b = new BoardService().selectOne(num);
+		ArrayList<Reply> replyList = new BoardService().selectReply(num);
+		
+		System.out.println("servlet:"+b);
+		
 		
 		String page = "";
-		System.out.println(b);
+		
 		
 		if(b != null) {
 			page = "views/board/information/informationView.jsp";
 			request.setAttribute("b", b);
+			request.setAttribute("r", replyList);
 		}else {
 			page = "views/common/errorPage.jsp";
 			request.setAttribute("msg", "게시판 상세 조회 실패!");
